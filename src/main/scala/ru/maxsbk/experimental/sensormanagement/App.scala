4bde88cb3969ebcab9782b6340369997b2fc212d
Max Shcherbakov <MaxSbk@mail.ru>
2020-10-21 21:55:53 +0400

    Fix something

--- src/main/scala/ru/maxsbk/experimental/sensormanagement/App.scala
+++ src/main/scala/ru/maxsbk/experimental/sensormanagement/App.scala
@@ -1,29 +1,28 @@
 package ru.maxsbk.experimental.sensormanagement
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import akka.stream.alpakka.influxdb.InfluxDbWriteMessage
 import akka.stream.alpakka.influxdb.scaladsl.InfluxDbFlow
-import akka.stream.scaladsl.{Sink, Source}
-import com.typesafe.config.ConfigFactory
-import org.influxdb.{InfluxDB, InfluxDBFactory}
+import akka.stream.scaladsl.{ Sink, Source }
+import com.typesafe.config.{ Config, ConfigFactory }
 import org.influxdb.dto.Point
+import org.influxdb.{ InfluxDB, InfluxDBFactory }
 
-import scala.util.{Failure, Random, Success}
+import scala.concurrent.ExecutionContextExecutor
+import scala.util.{ Failure, Random, Success }
 
 object App {
-  val AppConfig = ConfigFactory.load()
-  def apply() = {
+  val AppConfig: Config = ConfigFactory.load()
+  def apply(): Unit = {
     new App().startApp()
   }
 }
 
 class App {
   import App._
-  def startApp() = {
-    implicit val system = ActorSystem("sensors")
-    implicit val materializer = ActorMaterializer()
-    implicit val ec = system.dispatcher
+  def startApp(): Unit = {
+    implicit val system: ActorSystem          = ActorSystem("sensors")
+    implicit val ec: ExecutionContextExecutor = system.dispatcher
 
     implicit val influxDB: InfluxDB = InfluxDBFactory.connect(
       AppConfig.getString("influx_db.url"),
@@ -33,13 +32,14 @@ class App {
     val dbName = AppConfig.getString("influx_db.db_name")
     influxDB.setDatabase(dbName)
 
-    Source(Seq("sensor1", "sensor2", "sensor3")).map{sensorName =>
-      val p = Point.measurement("Temperature")
+    Source(Seq("sensor1", "sensor2", "sensor3")).map { _ =>
+      val p = Point
+        .measurement("Temperature")
         .addField("sensorType", "Simple Type")
-        .addField("temp", Random.nextDouble() * 100).build()
+        .addField("temp", Random.nextDouble() * 100)
+        .build()
       Seq(InfluxDbWriteMessage(p))
-    }
-      .via(InfluxDbFlow.create())
+    }.via(InfluxDbFlow.create())
       .runWith(Sink.seq)
       .onComplete {
         case Success(x) =>
@@ -49,4 +49,4 @@ class App {
   }
 }
 
-case class SensorData(sensorName: String, sensorType:String, meas: Double)
+case class SensorData(sensorName: String, sensorType: String, meas: Double)
