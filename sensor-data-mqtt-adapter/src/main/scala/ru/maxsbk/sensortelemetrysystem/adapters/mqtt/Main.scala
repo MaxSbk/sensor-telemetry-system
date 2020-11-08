b04fdbc23d537598c7017701c1a0cd7061858747
Max Shcherbakov <MaxSbk@mail.ru>
2020-11-08 19:36:49 +0400

    Add some code

--- sensor-data-mqtt-adapter/src/main/scala/ru/maxsbk/sensortelemetrysystem/adapters/mqtt/Main.scala
+++ sensor-data-mqtt-adapter/src/main/scala/ru/maxsbk/sensortelemetrysystem/adapters/mqtt/Main.scala
@@ -1,11 +1,39 @@
 package ru.maxsbk.sensortelemetrysystem.adapters.mqtt
 
+import akka.Done
+import akka.actor.ActorSystem
+import akka.stream.alpakka.mqtt.{MqttConnectionSettings, MqttQoS, MqttSubscriptions}
+import akka.stream.alpakka.mqtt.scaladsl.{MqttMessageWithAck, MqttSource}
+import akka.stream.scaladsl.{Sink, Source}
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+import ru.maxsbk.sensortelemetrysystem.adapters.mqtt.config.ProjectConfig
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
 object Main {
+  private val systemConfig                  = ProjectConfig()
+  implicit val system: ActorSystem          = ActorSystem("sensor-data-ingestion-system")
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
   def main(args: Array[String]) = {
     val connectionSettings = MqttConnectionSettings(
-      "tcp://localhost:1883", // (1)
-      "test-scala-client", // (2)
-      new MemoryPersistence // (3)
+      systemConfig.mqttBroker.url,
+      "test-scala-client",
+      new MemoryPersistence
     )
+
+    val testingTopic = "topic"
+
+    val mqttSource: Source[MqttMessageWithAck, Future[Done]] =
+      MqttSource.atLeastOnce(
+        connectionSettings
+          .withClientId(clientId = "source-spec/source1")
+          .withCleanSession(false),
+        MqttSubscriptions(testingTopic, MqttQoS.AtLeastOnce),
+        bufferSize = 8
+      )
+
+    mqttSource
+      .mapAsync(1)(messageWithAck => messageWithAck.ack().map(_ => messageWithAck.message))
+      .runWith(Sink.seq)
   }
 }
